d1de131854cf104620ffc31fd38d1595b1556cc5
William Liu <liuwilly4@gmail.com>
2022-01-17 18:39:28 -0500

    tailTest

--- tail.js
+++ tail.js
@@ -1,25 +1,14 @@
-var assertEqual = function(actual, expected) {
-  let equal = actual === expected;
-  console.log(`Assertion ${equal ? "Passed" : "Failed"}: ${actual} ${equal ? "=" : "!"}== ${expected}`);
-};
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
 
 var tail = (list) => {
-  let tailList=[];
-  for (let i = 1 ; i < list.length ; i ++ ){
-    tailList = [... tailList , list[i]];
+  let tailList = [];
+  for (let i = 1; i < list.length; i++) {
+    tailList = [...tailList, list[i]];
   }
 
   return tailList;
-
 };
 
-var result = tail (words);
 
-assertEqual(words.length, 3);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const tail = require("../tail");
+const assertEqual=require("../assertEqual");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+var result = tail(words);
+
+assertEqual(words.length, 3);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
