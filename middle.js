df3c31fe130998392fe8beca63bd5b9925aa7bd8
William Liu <liuwilly4@gmail.com>
2022-01-17 18:58:06 -0500

    middleTest.js

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-var eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-var assertArraysEqual = (array1, array2) => {
-  let equal = eqArrays(array1, array2);
 
-  console.log(
-    `Assertion ${equal ? "Passed" : "Failed"}: ${
-      equal ? "Array matched!" : `Array unmached!`
-    }`
-  );
-};
 
 var middle = (array) =>{
   let arrayLength = array.length
@@ -31,8 +14,4 @@ var middle = (array) =>{
   }  
 }
 
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4])
-
+module.exports=middle;
