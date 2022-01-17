082ed0e3f43dd1bb5f6549888ccd91eb13f7fd43
William Liu <liuwilly4@gmail.com>
2022-01-17 18:51:45 -0500

    asserArrayEqual require eqArray

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-var eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 var assertArraysEqual = (array1, array2) => {
   let equal = eqArrays(array1, array2);
@@ -14,3 +6,8 @@ var assertArraysEqual = (array1, array2) => {
   console.log(`Assertion ${equal ? "Passed" : "Failed"}: ${array1} ${equal ? "=" : "!"}== ${array2}`);
 };
 
+assertArraysEqual([1,2],[1,2]);
+assertArraysEqual([1,"a"],[1,2])
+
+
+module.exports = assertArraysEqual;
