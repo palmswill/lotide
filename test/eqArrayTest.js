12ff4d45aa91fe495f6236ce04c2600d94f923af
William Liu <liuwilly4@gmail.com>
2022-01-17 19:35:45 -0500

    dfdf

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,11 +1,6 @@
+const assertEqual = require("../assertEqual");
 const eqArrays=require("../eqArrays");
-const assert = require('chai').assert;
 
-describe("#eqArray", () => {
-  it(`returns false for [1, 2, 3], [1, 2, "3"]`, () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
-  });
-  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-  });
-});
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => should PASS
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,7 @@
 const middle = require("../middle");
-const expect = require("chai").expect;
+const assertArraysEqual=require("../assertArraysEqual");
 
-describe("#middle", () => {
-  it(`returns  [2] for [1, 2, 3]`, () => {
-    expect(middle([1, 2, 3])).deep.to.equal( [2]);
-  });
-  it(`returns  [3] for [1, 2, 3, 4, 5]`, () => {
-    expect(middle([1, 2, 3, 4, 5])).deep.to.equal( [3]);
-  });
-  it(`returns  [2, 3] for [1, 2, 3, 4]`, () => {
-    expect(middle([1, 2, 3, 4])).deep.to.equal( [2, 3]);
-  });
-  it(`returns  [3, 4] for [1, 2, 3, 4, 5, 6]`, () => {
-    expect(middle([1, 2, 3, 4, 5, 6])).deep.to.equal([3, 4]);
-  });
-});
+assertArraysEqual(middle([1, 2, 3]),[2])
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
+assertArraysEqual(middle([1, 2, 3, 4]),[2, 3])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4])
